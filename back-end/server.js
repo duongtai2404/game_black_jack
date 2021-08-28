c9769323b51e8ef0422ac18f2c50dea102458f21
duongtai2404 <maiduongtai.itnlu@gmail.com>
2021-08-28 13:58:48 +0700

    install cors body-parser

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`)
 })
