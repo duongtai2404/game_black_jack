17f5e5006b56c6c7f652548fb6b838aa2721f25d
duongtai2404 <maiduongtai.itnlu@gmail.com>
2021-08-30 11:14:57 +0700

    handle event save name in login  and check name localstorage in home

--- back-end/server.js
+++ back-end/server.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { text: 'hello world'});
-})
+  res.redirect('/home');
+});
 
-app.get('/player', (req,res) => {
-    res.render('player');
-})
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/player', (req, res) => {
+  res.render('player');
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`)
-})
+  console.log(`Server is listening at http://localhost:${PORT}`);
+});
